68946722e14b622e8bc0aa4821eb05907768c7c9
Kora1006 <471209058@qq.com>
2019-10-11 21:19:46 +0800

    完成首页

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,200 @@
 <template>
-  <div>
-    网页首页
+  <div class="container">
+    <el-carousel :interval="5000" arrow="always">
+      <el-carousel-item
+        v-for="(item,index) in banners"
+        :key="index"
+        :style="`
+                background:url(${$axios.defaults.baseURL+item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+      ></el-carousel-item>
+    </el-carousel>
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 flex可以兼容 -->
+        <el-row type="flex" class="search-tab">
+          <span
+            v-for="(item, index) in options"
+            :key="index"
+            :class="{active: index === currentOption}"
+            @click="handleOption(index)"
+          >
+            <i>{{item.name}}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input
+            :placeholder="options[currentOption].placeholder"
+            v-model="searchValue"
+            @keyup.enter="handleSearch"
+          />
+          <i class="el-icon-search" @click="handleSearch"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      banners: [],
+      options: [
+        {
+          name: "攻略",
+          placeholder: "搜索城市",
+          pageUrl: "/post?city="
+        },
+        {
+          name: "酒店",
+          placeholder: "请输入城市搜索酒店",
+          pageUrl: "/hotel?city="
+        },
+        {
+          name: "机票",
+          placeholder: "请输入出发地",
+          pageUrl: "/air"
+        }
+      ],
+      currentOption: 0, //搜索导航栏选中的选项
+      searchValue: "" //搜索框的值
+    };
+  },
+  mounted() {
+    this.$axios({
+      url: "/scenics/banners",
+      method: "GET"
+    }).then(res => {
+      // console.log(res)
+      const { data } = res.data;
+      this.banners = data;
+    });
+  },
+  methods: {
+    // 搜索、
+    handleSearch() {
+      const item = this.options[this.currentOption];
+      // 跳转到对应的页面
+      return this.$router.push(item.pageUrl + this.searchValue);
+    },
+    //切换tab栏
+    handleOption(index) {
+      this.currentOption = index;
+      const item = this.options[index];
+      if (item.name === "机票") {
+        return this.$router.push(item.pageUrl);
+      }
+    }
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
 
+  /deep/ .el-carousel__container {
+    height: 700px;
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+
+  .banner-content {
+    z-index: 9;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
+
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
+
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
+  }
+}
 </style>
