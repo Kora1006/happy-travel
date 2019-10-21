440ea1f9bf1ee27c13b88800db739f81b30835f1
CJChans <52377346+CJChans@users.noreply.github.com>
2019-10-21 17:08:07 +0800

    面包屑和搜索框基本布局完成

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,36 +1,32 @@
 <template>
-  <div>
-    <!-- 高德地图 -->
-    <div id="map-wrap"></div>index
-  </div>
+    <div>
+        
+       <div>
+           <HotelDetail/>
+       </div>
+    </div>
 </template>
 
 <script>
+// 引入酒店详情组件
+import HotelDetail from "@/components/hotel/hotelDetail"
 export default {
-  mounted() {
-    window.onLoad = function() {
-      var map = new AMap.Map("map-wrap");
-    };
-    var url =
-      "https://webapi.amap.com/maps?v=1.4.15&key=f1357cd6a96013e9d92c0c450811e88d&callback=onLoad";
-    var jsapi = document.createElement("script");
-    jsapi.charset = "utf-8";
-    jsapi.src = url;
-    document.head.appendChild(jsapi);
+    //注册组件
+    components:{
+        HotelDetail
+    },
 
-    this.$axios({
-      url:'/cities',
-      params:{
-        name:'上海'
-      }
-
-    }).then(res=>{
-      console.log(res)
-    })
-  }
-};
+    //请求酒店数据
+    mounted(){
+        this.$axios({
+            url:"/hotels",
+        }).then(res=>{
+           console.log(res)
+        })
+    }
+}
 </script>
 
-<style lang="less" scoped>
-#map-wrap {width:300px; height: 180px; }  
+<style>
+
 </style>
