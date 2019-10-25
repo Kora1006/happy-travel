3dd119a73f6d8182b39fe3b0eb7b4a700aca2bfb
Kora1006 <471209058@qq.com>
2019-10-25 14:50:55 +0800

    [master][fix]修复酒店主页无法显示

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,48 +1,43 @@
 <template>
+  <div>
+    <!-- 酒店详情组件 -->
     <div>
-            <!-- 酒店详情组件 -->
-        <div>
-            <HotelDetail/>
-        </div>
-
-        <!-- 酒店列表组件 -->
-        <div>
-            <HotelList/>
-        </div>       
+      <HotelDetail />
     </div>
+
+    <!-- 酒店列表组件 -->
+    <div>
+      <HotelList />
+    </div>
+  
+  </div>
 </template>
 
 <script>
 // 引入酒店详情组件
-import HotelDetail from "@/components/hotel/hotelDetail"
-import HotelList from "@/components/hotel/hotelList"
+import HotelDetail from "@/components/hotel/hotelDetail";
+import HotelList from "@/components/hotel/hotelList";
 export default {
-    //注册组件
-    components:{
-        HotelDetail,
-        HotelList
-    },
+  //注册组件
+  components: {
+    HotelDetail,
+    HotelList
+  },
 
-    data() {
-        return {
-
-        }
-        },
-    //请求酒店数据
-    mounted(){
-        this.$axios({
-            url:"/hotels",
-        }).then(res=>{
-           console.log(res)
-        })
-    },
-    methods:{
-
-    }
-
-}
+  data() {
+    return {};
+  },
+  //请求酒店数据
+  mounted() {
+    this.$axios({
+      url: "/hotels"
+    }).then(res => {
+      console.log(res);
+    });
+  },
+  methods: {}
+};
 </script>
 
 <style>
-
 </style>
